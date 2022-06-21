013bc2dc07f6198aca3b1f2d80f59fa04009008d
pabsan-0 <psantana@catec.aero>
2022-06-21 15:00:03 +0200

    typo in/out

--- post-checks.sh
+++ post-checks.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 echo "Diagnosing input dataset..."
-echo "Found $(ls dataset_out/images/*/*.jpg | wc -l) images."
-echo "Found $(ls dataset_out/labels/*/*.txt | wc -l) labels."
-echo "Found $(find dataset_out -name '*.txt' -size 0 | wc -l) empty labels."
+echo "Found $(ls dataset_in/images/*/*.jpg | wc -l) images."
+echo "Found $(ls dataset_in/labels/*/*.txt | wc -l) labels."
+echo "Found $(find dataset_in -name '*.txt' -size 0 | wc -l) empty labels."
 echo "WARNING: by default, I will not include nonlabelled images in your output data."
 
 echo "Diagnosing converted dataset..."
